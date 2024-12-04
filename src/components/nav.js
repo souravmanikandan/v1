67475c07ce7cda5054e0bbe718218784b2dd8bfd
smcoderr <souravoz2018@gmail.com>
2024-12-04 20:52:59 +0530

    nine

--- src/components/nav.js
+++ src/components/nav.js
@@ -206,7 +206,11 @@ const Nav = ({ isHome }) => {
   );
 
   const ResumeLink = (
-    <a className="resume-button" href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+    <a
+      className="resume-button"
+      href="https://souravmanikandan.github.io/v1/resume.pdf"
+      target="_blank"
+      rel="noopener noreferrer">
       Resume
     </a>
   );
